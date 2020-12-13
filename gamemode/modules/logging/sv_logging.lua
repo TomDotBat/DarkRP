93310df4425e1fd83a794c777cbaec022a111da2
TomDotBat <tom@tomdotbat.dev>
2020-12-13 16:44:42 +0000

    Disable DarkRP file logs

--- gamemode/modules/logging/sv_logging.lua
+++ gamemode/modules/logging/sv_logging.lua
@@ -16,22 +16,8 @@ local function AdminLog(message, colour, allowedPlys)
     umsg.End()
 end
 
-local DarkRPFile
 function DarkRP.log(text, colour, noFileSave)
     if colour then
         CAMI.GetPlayersWithAccess("DarkRP_SeeEvents", fp{AdminLog, text, colour})
     end
-
-    if not GAMEMODE.Config.logging or noFileSave then return end
-
-    if not DarkRPFile then -- The log file of this session, if it's not there then make it!
-        if not file.IsDir("darkrp_logs", "DATA") then
-            file.CreateDir("darkrp_logs")
-        end
-
-        DarkRPFile = "darkrp_logs/" .. os.date("%m_%d_%Y %I_%M %p") .. ".txt"
-        file.Write(DarkRPFile, os.date() .. "\t" .. text)
-        return
-    end
-    file.Append(DarkRPFile, "\n" .. os.date() .. "\t" .. (text or ""))
 end
