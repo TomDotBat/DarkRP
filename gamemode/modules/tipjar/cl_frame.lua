652e95289274916f976d81596ab1fcb6ec2b7e51
blu <blu@bluiscool.dev>
2021-06-05 19:00:37 -0500

    Change to a generic name

--- gamemode/modules/tipjar/cl_frame.lua
+++ gamemode/modules/tipjar/cl_frame.lua
@@ -287,10 +287,10 @@ onModelUpdate("frameVisible", function(visible)
 end)
 
 function DarkRP.tipJarUI(tipjar)
-    if ATOMIC.TipjarFrame then ATOMIC.TipjarFrame:Remove() end
-    ATOMIC.TipjarFrame = vgui.Create("ATOMIC.Tipjar")
+    if PIXEL.TipjarFrame then PIXEL.TipjarFrame:Remove() end
+    PIXEL.TipjarFrame = vgui.Create("PIXEL.Tipjar")
+
     if true then return end
-    print("BRUGER")
     updateModel("tipjar", tipjar)
     updateModel("isOwner", tipjar:Getowning_ent() == LocalPlayer())
     updateModel("amount", 0)
